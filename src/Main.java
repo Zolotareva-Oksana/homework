2fdfd8992d47ad44d04f00bfe083fb8c6ba6b8a8
Zolotareva-Oksana <zolotoksana@yandex.ru>
2025-03-14 02:30:20 +0600

    DZ-12 (методы объектов)

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,15 @@ public class Main {
 
        Book test = new Book("Тест", zon, 2025);
        Book dto = new Book("Детство", tln, 1852);
+       System.out.println(test);
+       System.out.println(dto);
 
-       test.fullInformationBook();
-       System.out.println();
-       dto.fullInformationBook();
-       System.out.println();
        dto.setYearPublication(2024);
-       dto.fullInformationBook();
+       System.out.println(dto);
+
+       System.out.println(test.getAuthor().equals(dto.getAuthor()));
+       System.out.println(test.getYearPublication() == (dto.getYearPublication()));
+       System.out.println(test.getTitle().equals(dto.getTitle()));
+       System.out.println(dto.getTitle().equals(dto.getTitle()));
     }
 }
